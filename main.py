f13bb5511b49fbb46bc3d81d58bc369e40ded197
rich-coleman-gh <letitbleed9@gmail.com>
2018-11-07 18:14:35 -0500

    added pearson correlations to numeric data

--- main.py
+++ main.py
@@ -14,16 +14,24 @@ corr = data.corr()
 #sns.pairplot(data)
 columns_to_examine = ['OverallQual', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'GrLivArea', 'GarageCars', 'GarageArea', 'TotalBsmtSF', '1stFlrSF']
 
-# examine feature data types
-column_types = data.columns.groupby(data.dtypes)
-column_types = {k.name: v for k, v in column_types.items()}
-
+# group features by data type
 numeric_data = data.select_dtypes(include='number')
 categorical_data = data.select_dtypes(include='object')
+#categorical_data = categorical_data.astype(str)
 
-
-# examine correlation and p value of each feature against sales price
-pearson_correlations = {}
-for i in columns_to_examine:
-	corr, p_value = pearsonr(data[i], data['SalePrice'])
+# examine correlation and p value of each numerical feature against sales price
+pearson_correlations = {"column" : ["correlation", "p_value"]}
+for i in numeric_data.columns:
+	corr, p_value = pearsonr(numeric_data[i], data['SalePrice'])
 	pearson_correlations[i] = [corr, p_value]
+
+# convert dictionary to dataframe for export
+pearson_correlations = pd.DataFrame.from_dict(pearson_correlations)
+pearson_correlations.to_csv("~/Documents/Git/kaggle_housing_prices_advanced_regression_techniques/pearson_correlations.csv")
+
+# examine correlation and p value of each categorical feature against sales price
+spearman_correlations = {"column" : ["correlation", "p_value"]}
+for i in categorical_data.columns:
+	corr, p_value = spearmanr(categorical_data[i], data['SalePrice'])
+	spearman_correlations[i] = [corr, p_value]
+
